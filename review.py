f3bc3621436da82ce61e3568846aa119b1f2e903
Anatoliy Kolodkin <anatoliy@ankosoftware.com>
2023-10-12 08:27:20 -0500

    updated promt

--- review.py
+++ review.py
@@ -149,7 +149,7 @@ def get_review_from_openai(patch):
   model = "gpt-3.5-turbo"
   patch_tokens = 1000  # need to calcualte tokens
 
-  question = "Code Review Request for Bugs, Issues, or Standard Violations, lint errors, Dont check for unused. Say LGTM if no issues found. \n"
+  question = "Review this code diff for bugs, issues, lint errors, but ignore unused variables, say simply LGTM if no issues found: \n"
   messages =  [{"role": "user",  "content": question + patch}]
 
   response = openai.ChatCompletion.create(
@@ -189,7 +189,7 @@ def get_review():
   patch_contents = split_patch_file_content(patch)
 
   for file_patch, line_number in patch_contents:
-    question = "Code Review Request for Bugs, Issues, or Standard Violations, lint errors, Dont check for unused. Say LGTM if no issues found.. \n"
+    question = "Review this code diff for bugs, issues, lint errors, but ignore unused variables, say simply LGTM if no issues found: \n"
     prompt = question + file_patch
 
     response = openai.Completion.create(
